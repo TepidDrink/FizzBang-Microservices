3a835434864e22c4c6e2122c485aaf5eec0ed57f
Nicholas Sumner <NikSum15@gmail.com>
2022-02-14 22:10:42 +0000

    refactor fizzbang logic to reduce number of if's

--- src/main/java/com/personal/fizzbang/api/FizzBangService.java
+++ src/main/java/com/personal/fizzbang/api/FizzBangService.java
@@ -5,14 +5,13 @@ public class FizzBangService {
     static final String BANG = "bang";
 
     public String fizzBangTheNumber(int input) {
-        if (input % 15 == 0) {
-            return FIZZ + BANG;
-        } else if (input % 3 == 0) {
-            return FIZZ;
-        } else if (input % 5 == 0) {
-            return BANG;
-        } else {
-            return null;
+        String output = "";
+        if (input % 3 == 0) {
+            output += FIZZ;
         }
+        if (input % 5 == 0) {
+            output += BANG;
+        }
+        return !output.equals("") ? output : null;
     }
 }
