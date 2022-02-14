6c08a3f794fbc0279b8a54429a2028aea59b63df
Nicholas Sumner <NikSum15@gmail.com>
2022-02-14 22:05:52 +0000

    15 now returns fizzbang

--- src/main/java/com/personal/fizzbang/api/FizzBangService.java
+++ src/main/java/com/personal/fizzbang/api/FizzBangService.java
@@ -5,7 +5,9 @@ public class FizzBangService {
     static final String BANG = "bang";
 
     public String fizzBangTheNumber(int input) {
-        if (input % 3 == 0) {
+        if (input % 15 == 0) {
+            return FIZZ + BANG;
+        } else if (input % 3 == 0) {
             return FIZZ;
         } else if (input % 5 == 0) {
             return BANG;
