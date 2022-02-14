2cfec5f8bb0be550531e015d0709185c30f5ff67
Nicholas Sumner <NikSum15@gmail.com>
2022-02-14 22:00:07 +0000

    3 and 6 returning fizz

--- src/main/java/com/personal/fizzbang/api/FizzBangService.java
+++ src/main/java/com/personal/fizzbang/api/FizzBangService.java
@@ -5,7 +5,7 @@ public class FizzBangService {
     static final String BANG = "bang";
 
     public String fizzBangTheNumber(int input) {
-        if (input == 3) {
+        if (input % 3 == 0) {
             return FIZZ;
         } else if (input == 5) {
             return BANG;
