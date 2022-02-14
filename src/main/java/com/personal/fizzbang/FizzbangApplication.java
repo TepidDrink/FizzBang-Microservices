7ce550dec0338b6245d94236af25719137e3a201
Nicholas Sumner <NikSum15@gmail.com>
2022-02-14 19:20:04 +0000

    Initialised Repo

---
+++ src/main/java/com/personal/fizzbang/FizzbangApplication.java
@@ -0,0 +1,13 @@
+package com.personal.fizzbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbangApplication.class, args);
+	}
+
+}
